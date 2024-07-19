ec680d57c03751bf3f1653fc5ca874de1fe1b9a9
sirons84 <62355167+sirons84@users.noreply.github.com>
2024-07-20 05:00:26 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ openai.api_key = API_KEY
 # 페이지 제목
 st.header("강점 중심의 학생 주도성 신장을 위한 ", divider='rainbow')
 
-st.write(':gray[울산 화진초등학교] :blue[석희철] :sunglasses:')
+st.subtitle(':gray[울산 화진초등학교] :blue[석희철] :sunglasses:')
 
-st.write('학생 맥락과 AIDT 기능, 그리고 AI 기반 지원 방안을 단계적으로 입력해주세요.', divider='rainbow')
+st.write("학생 맥락과 AIDT 기능, 그리고 AI 기반 지원 방안을 단계적으로 입력해주세요.", divider='rainbow')
 
 # Step 1: 학생 맥락 입력
 st.subheader("STEP 1. 학생 맥락")
